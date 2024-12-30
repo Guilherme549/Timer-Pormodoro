916e82fef838f8f1fefe1a2ed901d7b1bb69fc54
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-12-29 22:00:45 -0300

    eslint

--- timer pomodoro/src/@types/styled.d.ts
+++ timer pomodoro/src/@types/styled.d.ts
@@ -1,5 +1,5 @@
-import 'styled-components'
-import { defaultTheme } from '../styles/themes/default'
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
 
 type ThemeType = typeof defaultTheme;
 
@@ -7,3 +7,4 @@ type ThemeType = typeof defaultTheme;
 declare module 'styled-components' {
     export interface DefaultTheme extends ThemeType {}
 }
+
